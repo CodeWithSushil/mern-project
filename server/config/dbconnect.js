3fb27e3680cf621dcffd34c392c830b0ffdcc91c
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-09-21 15:43:45 +0000

    update and mongodb connection setup

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,2 +1,19 @@
-const db = require("mongoose");
-const PORT = 5000;
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { MONGO_USER, MONGO_PASS, MONGO_HOST, MONGO_PORT, MONGO_DB } =
+    process.env;
+
+const uri = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        process.exit(1);
+    }
+};
