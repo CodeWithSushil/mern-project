c0583d2fab8002e79a46a217be2421313b48b95f
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-09-23 18:19:47 +0530

    update

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import mariadb from "mariadb";
+
+dotenv.config();
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  connectionLimit: 10
+});
+
+export default pool;
---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import pool from "./config/db.js";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    // res.writeHead(200, {'Content-Type':
+'text/html'});
+    res.send('<h1> Welcome to MERN Stack! </h1>');
+});
+
+/*
+ app.get("/", async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT NOW() AS now");
+    res.json(rows[0]);
+  } catch (err) {
+    console.error("❌ Database error:", err);
+    res.status(500).send("Database error");
+  } finally {
+    if (conn) conn.release(); // always release back to pool
+  }
+});
+*/
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
+});
