5588ec3b675481f00bedc1ab351cf0fb8a722878
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-09-24 17:07:54 +0530

    update

--- server/server.js
+++ server/server.js
@@ -3,14 +3,13 @@ import session from "express-session";
 import cookie from "cookie-parser";
 
 import pool from "./config/db.js";
+import router from "./routes/auth.route.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", async (req, res) => {
-    res.send('<h1>Welcome to MERN Stack!</h1>');
-});
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
