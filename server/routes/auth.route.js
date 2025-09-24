c199e34483c3752840e207a7560d92746a39d37d
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-09-24 17:08:09 +0530

    update

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,4 +1,20 @@
-import { Router } from "express";
+// import { express, Router } from "express";
+import express from 'express';
+import { Router } from 'express';
 
-const router = new Router();
+// const { express, Router } = pkg;
 
+const router = Router();
+const app = express();
+
+router.get("/", async (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+});
+
+router.get("/about", async (req, res) => {
+    res.send('<h1>About Page</h1>');
+});
+
+// console.log(router);
+
+export default router;
